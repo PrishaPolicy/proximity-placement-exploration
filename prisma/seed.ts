9bd93aeff708b1559647a7e43147e760f1fab115
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2023-03-31 12:32:25 +0530

    added database string

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,70 +1,49 @@
-import { PrismaClient, Prisma } from '@prisma/client'
+import { PrismaClient, Prisma } from "@prisma/client";
+import {
+  randUser,
+  randCatchPhrase,
+  randLines,
+  randBoolean,
+} from "@ngneat/falso";
+const prisma = new PrismaClient();
 
-const prisma = new PrismaClient()
-
-const userData: Prisma.UserCreateInput[] = [
-  {
-    name: 'Alice',
-    email: 'alice@prisma.io',
+const userData: Prisma.UserCreateInput[] = [...Array(1000)].map((_, i) => {
+  const data = randUser();
+  return {
+    name: data.username,
+    email: data.email,
     posts: {
-      create: [
-        {
-          title: 'Join the Prisma Slack',
-          content: 'https://slack.prisma.io',
-          published: true,
-        },
-      ],
+      create: [...Array(10)].map(() => {
+        return {
+          title: randCatchPhrase(),
+          content: randLines(),
+          published: randBoolean(),
+        };
+      }),
     },
-  },
-  {
-    name: 'Nilu',
-    email: 'nilu@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Follow Prisma on Twitter',
-          content: 'https://www.twitter.com/prisma',
-          published: true,
-        },
-      ],
-    },
-  },
-  {
-    name: 'Mahmoud',
-    email: 'mahmoud@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Ask a question about Prisma on GitHub',
-          content: 'https://www.github.com/prisma/prisma/discussions',
-          published: true,
-        },
-        {
-          title: 'Prisma on YouTube',
-          content: 'https://pris.ly/youtube',
-        },
-      ],
-    },
-  },
-]
+  };
+});
 
 async function main() {
-  console.log(`Start seeding ...`)
-  for (const u of userData) {
-    const user = await prisma.user.create({
-      data: u,
+  console.log(`Start seeding ...`);
+
+  await Promise.all(
+    userData.map(async (u) => {
+      const user = await prisma.user.create({
+        data: u,
+      });
+      console.log(`Created user with id: ${user.id}`);
     })
-    console.log(`Created user with id: ${user.id}`)
-  }
-  console.log(`Seeding finished.`)
+  );
+  console.log(`Seeding finished.`);
 }
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
