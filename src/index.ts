bf14f8e6b50a163e1eff2b3188cfaad4d8dd4a23
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2023-05-03 16:30:47 +0530

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
 
 app.get("/get-data", async (req, res) => {
   const t1 = new Date().getTime();
-  const result = await prisma.post.findMany({
-    include: {
-      author: true,
-    },
-  });
+  // const result = await prisma.post.findMany({
+  //   include: {
+  //     author: true,
+  //   },
+  // });
   const t2 = new Date().getTime();
-  return res.json({ timeTaken: `${t2 - t1}ms`, data: result });
+  return res.json({ timeTaken: `${t2 - t1}ms` });
 });
 
 const server = app.listen(3000, () =>
